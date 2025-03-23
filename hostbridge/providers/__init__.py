adf215e0d502fb6e9adad8429064aa2b83de89ce
elblanco2 <122809544+elblanco2@users.noreply.github.com>
2025-03-23 17:02:27 -0400

    Add hosting provider module

---
+++ hostbridge/providers/__init__.py
@@ -0,0 +1,16 @@
+"""
+Hosting provider abstractions for deployment operations.
+"""
+
+from .base import HostingProvider, HostingProviderFactory
+from .netlify import NetlifyProvider
+from .shared_hosting import SharedHostingProvider
+from .vercel import VercelProvider
+
+__all__ = [
+    "HostingProvider",
+    "HostingProviderFactory",
+    "NetlifyProvider",
+    "SharedHostingProvider",
+    "VercelProvider",
+]
